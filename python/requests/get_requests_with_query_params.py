cd31e1630614a7d23d658597957b7330f24fec9e
Julien Nicolas <ju.nicodev@gmail.com>
2023-08-09 12:11:22 +0200

    python - show how to send a get request properly

--- python/requests/get_requests_with_query_params.py
+++ python/requests/get_requests_with_query_params.py
@@ -1,13 +1,47 @@
-"""" HTTP GET Requests With Query Params """
-import requests
+"""" HTTP GET Requests With Query Params.
 
-url = 'http://something.com'
+requests.get is equivalent to requests.request(method="GET").
+"""
+import requests
+import logging as log
+import sys
+
+url = 'https://example.com'
+url = "https://blah.com"
 params = {
     'param_1': 'value_1',
     'param_2': 'value_2',
 }
+timeout = 2 # In seconds
 
-response = requests.get(url, params)
+# Timeout is in seconds
+try:
+    response = requests.get(url, params=params, timeout=timeout)
+    # Raises a HTTPError if r.status_code >= 400 
+    response.raise_for_status()
+except requests.ConnectionError:
+    log.error("couldn't connect to server")
+    sys.exit(1)
+except requests.HTTPError:
+    log.error(f"status code == {response.status_code}")
+    sys.exit(1)
+except requests.TooManyRedirects:
+    log.error("too many redirects to reach remote host")
+    sys.exit(1)
+except requests.Timeout:
+    log.error("request timed out after {timeout} seconds")
+    sys.exit(1)
 
-# Show response with JSON body
-print(response.json())
+# If the content-type is unknown, check it using http headers!
+print(f"content-type == {response.headers['Content-Type']}")
+
+## Get content as bytes
+#print(response.content)
+
+## Show response as a raw str
+print(response.text)
+
+## Show response with JSON body
+# raise a JSONDecodeError if the content is
+# not in json
+#print(response.json())
