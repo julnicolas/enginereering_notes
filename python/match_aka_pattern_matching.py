68606b27553b2096b3621cf2ec3ed46fff788453
Julien Nicolas <ju.nicodev@gmail.com>
2023-10-17 18:45:13 +0200

    python - improve notes on pattern matching

--- python/match_aka_pattern_matching.py
+++ python/match_aka_pattern_matching.py
@@ -4,6 +4,7 @@ match syntax. """
 lst = [
     ("Sonic", {"speed": 100, "strength": 45}),
     ("Knuckles", {"speed": 75, "strength": 90}),
+    ("Shadow", "Rouge", "Tails", "Amy"),
 ]
 
 for e in lst:
@@ -13,6 +14,14 @@ for e in lst:
             print("This is Sonic!")
         case (_, {"strength": 90}):
             print("This is a strong character!")
+        case ("Sonic", "Knuckles", *others):
+            # Pack and unpack tuple elements in 'others' var
+            print("Found Sonic, Knuckles and others are:", *others)
+        case ("Sonic" | "Shadow", "Rouge", *others) as v:
+            # Pack and unpack tuple elements in 'others' var
+            # Defining v is useful if a complex succession of functions is applied
+            # to 'e'
+            print("Happy bunch: ", v)
         case _:
             print("this matches every character, let's print it out!")
             print(e)
