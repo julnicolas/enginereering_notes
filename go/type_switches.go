a1404076aec877d987c9f97824c3cedafba42bbc
Julien Nicolas <ju.nicodev@gmail.com>
2024-02-05 18:46:37 +0100

    go - type switch

---
+++ go/type_switches.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var unknown interface{} = "hello"
+
+	switch v := unknown.(type) {
+		case int:
+			fmt.Printf("int type, value == %d\n", v)
+		case string:
+			fmt.Printf("string type, value == %s\n", v)
+		case interface{}:
+			fmt.Println("empty interface")
+	}
+}
