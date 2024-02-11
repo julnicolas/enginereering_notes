3a197a6822efd1dbea05d601c589377a13a337a3
Julien Nicolas <ju.nicodev@gmail.com>
2024-02-11 19:09:04 +0100

    go - improve documentation

--- go/generics/builtin_constraints.go
+++ go/generics/builtin_constraints.go
@@ -7,10 +7,12 @@ Builtins constraints are the following:
 - any (defined in builtins.go as type any = interface{})
 - comparable (defined in builtins.go as type comparable = interface{ comparable })
 	comparable types are compatible with <, >, and == operators
-- any type defined in the 'constraints' package. It defines:
+- any type defined in the 'x/exp/constraints' package. It defines:
 	- numeric types such as Int for ~int | ~int8 | ~int16 ... 
 	- the Ordered type which refers to types having < and > operator
 	  but not necessarily the == operator.
+	- Note: the x/exp path contains packages that are under adoption tests
+		they can either be integrated later in the std lib or simply be left as is.
 */
 
 // Any returns true if at least one entry in s1 verifies the predicate
