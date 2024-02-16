af1b0f26e1d3968d5bf2bbee09a01592c8a4dbe2
Julien Nicolas <ju.nicodev@gmail.com>
2024-02-16 14:36:43 +0100

    go - read write channels as func params

---
+++ go/channels/read_write_only_as_function_params.go
@@ -0,0 +1,50 @@
+package main
+
+/* This program shows how to pass channels
+to functions but specifying only one of read
+or write direction (sending or receiving end).
+
+It is better to specify it so that compiler can
+help prevent unintended behaviours.
+
+*/
+
+import "fmt"
+
+// ReadWrite can both read and write to channel c
+func ReadWrite(c chan int) {}
+
+// ReadOnly receives a read only chan and reads its content
+func ReadOnly(c <-chan int) {
+	for i := range c {
+		fmt.Printf("received %d\n", i)
+	}
+}
+
+// Type used to signal waiting for channels is over
+// Only one value is possible - StopSignal{}
+type StopSignal struct{}
+
+// WriteOnly receives two write only channels
+// It first writes content to c, then when over,
+// sends to StopSignal
+func WriteOnly(c chan<- int, stop chan<- StopSignal) {
+	ints := []int{2, 4, 6}
+	for _, v := range ints {
+		fmt.Printf("writing %d\n", v)
+		c <- v
+	}
+
+	stop <- StopSignal{}
+}
+
+func main() {
+	c := make(chan int)
+	defer close(c)
+	stop := make(chan StopSignal)
+	defer close(stop)
+
+	go ReadOnly(c)
+	go WriteOnly(c, stop)
+	<- stop
+}
