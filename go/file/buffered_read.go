f12c52ce1d51a4b7885a0a548527c732111b0c5f
Julien Nicolas <ju.nicodev@gmail.com>
2024-03-02 19:41:45 +0100

    go - add comment on limitation

--- go/file/buffered_read.go
+++ go/file/buffered_read.go
@@ -20,6 +20,14 @@ func main() {
 
     var line string
     r := bufio.NewScanner(f)
+
+	// Returns False when EOF has been reached
+	// If it has been reached once but then the file has content added to it
+	// again then, Scan will still return False.
+	//
+	// Therefore to tail a file one of the aspect to keep in mind is to check the amount
+	// of read bytes... given the file hasn't been overwritten! It is preferable to use the
+	// tail package for that effect then.
     for r.Scan() {
         line = r.Text()
         fmt.Println(line)
