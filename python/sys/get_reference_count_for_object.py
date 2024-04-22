a42992a7c7cdb1f3a9f7ae191c694ef5bd2c4694
Julien Nicolas <ju.nicodev@gmail.com>
2024-04-22 11:27:35 +0200

    python - improve note

--- python/sys/get_reference_count_for_object.py
+++ python/sys/get_reference_count_for_object.py
@@ -3,16 +3,18 @@
 from sys import getrefcount
 
 
-def count(obj): 
-    print(getrefcount(obj)) # a new ref is created here
+def count(obj):
+    print(getrefcount(obj))  # a new ref is created here
 
 
-l = [] # the object is created here
-count(l) # a new ref is created here when calling
+l = []  # the object is created here
+count(l)  # a new ref is created here when calling
 # hence the result being 3
 
 ll = l
 count(l)
 
+# ref count is decremented by explicitely
+# calling del or if ref goes out of scop
 del ll
 count(l)
